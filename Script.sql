4190f9d86e4342183f62312eec9b5aa895b461fb
Gabriel Gouveia <jose.gabriel@smn.com.br>
2023-02-16 09:42:04 -0300

    Pequenas Correcoes, 2 Procedures semi completas

--- Script.sql
+++ Script.sql
@@ -328,18 +328,18 @@ INSERT INTO conta VALUES
 
 INSERT INTO emprestimo VALUES 
 (2, 1000, 10, 100, '20190910', 2, '20220226 14:34:00', NULL, NULL),
-(1, 1000, 3, 200, '20220802', 2, '20220228 16:01:00', NULL, NULL),
-(3, 6000, 5, 160, '20150924', 3, '20220301 12:30:00', NULL, NULL),
-(1, 9000, 9, 100, '20130917', 2, '20220306 09:08:00', NULL, NULL),
-(2, 7000, 5, 140, '20180523', 3, '20220630 15:25:00', NULL, NULL),
-(3, 7000, 12, 580, '20190911', 4, '20221218 11:35:00', NULL, NULL),
-(2, 5000, 5, 100, '20220930', 4, '20230104 06:32:00', NULL, NULL),
-(3, 9500, 8, 118, '20210502', 3, '20210302 08:43:00', NULL, NULL),
-(2, 7050, 5, 150, '20200512', 2, '20230806 09:34:00', NULL, NULL),
-(1, 1000, 10, 100, '20230430', 4, '20220512 13:45:00', NULL, NULL),
-(1, 4900, 4, 100, '20230215', 2, '20220709 12:59:00', NULL, NULL),
-(2, 8950, 5, 170, '20200518', 3, '20210614 22:03:00', NULL, NULL),
-(3, 6900, 4, 200, '20210918', 4, '20190719 20:28:00', NULL, NULL),
+(1, 1000, 5, 200, '20220802', 2, '20220228 16:01:00', NULL, NULL),
+(3, 4000, 5, 800, '20150924', 3, '20220301 12:30:00', NULL, NULL),
+(1, 1000, 2, 500, '20130917', 2, '20220306 09:08:00', NULL, NULL),
+(2, 8000, 16, 500, '20180523', 3, '20220630 15:25:00', NULL, NULL),
+(3, 16000, 10, 160, '20190911', 4, '20221218 11:35:00', NULL, NULL),
+(2, 4000, 2, 2000, '20220930', 4, '20230104 06:32:00', NULL, NULL),
+(3, 2000, 8, 250, '20210502', 3, '20210302 08:43:00', NULL, NULL),
+(2, 2000, 5, 400, '20200512', 2, '20230806 09:34:00', NULL, NULL),
+(1, 8000, 10, 800, '20230430', 4, '20220512 13:45:00', NULL, NULL),
+(1, 16000, 4, 4000, '20230215', 2, '20220709 12:59:00', NULL, NULL),
+(2, 8000, 5, 1600, '20200518', 3, '20210614 22:03:00', NULL, NULL),
+(3, 2000, 4, 500, '20210918', 4, '20190719 20:28:00', NULL, NULL),
 (4, 0.00, 0, 0.00, '20220105', 2, '20220306 09:08:00', NULL, NULL),
 (6, 0.00, 0, 0.00, '20220105', 3, '20220630 15:25:00', NULL, NULL),
 (7, 0.00, 0, 0.00, '20220105', 2, '20220829 12:39:00', NULL, NULL),
@@ -562,7 +562,7 @@ GO
 
 
 
----------------------------------------SP EXTRATO CLIENTE SMN--------------------------------------------
+---------------------------------------SP EXTRATO CLIENTE BANCO--------------------------------------------
 IF EXISTS(SELECT TOP 1 1 FROM sysobjects WHERE ID = object_id(N'[SP.ExtratoConta]') AND objectproperty(ID,N'isProcedure') = 1)
 	DROP PROCEDURE [SP.ExtratoConta]	
 
@@ -595,7 +595,7 @@ CREATE PROCEDURE [SP.ExtratoConta]
 		ON ct.id_cliente = cl.id
 		INNER JOIN tipo_movimentacao tm
 		ON tm.id = ex.id_tipo_movimentacao
-		WHERE cl.id = @id_cliente
+		WHERE cl.id = @id_cliente;
 		RETURN 0
 	END
 GO
@@ -609,8 +609,8 @@ GO
 
 
 
----------------------------------------SP INADIMPLENCIA CLIENTE SMN--------------------------------------------
-IF EXISTS(SELECT TOP 1 1 FROM sysobjects WHERE ID = object_id(N'[SP.Inadiplencia]') AND objectproperty(ID,N'isProcedure') = 1)
+---------------------------------------SP INADIMPLENCIA CLIENTE BANCO--------------------------------------------
+IF EXISTS(SELECT TOP 1 1 FROM sysobjects WHERE ID = object_id(N'[SP.Inadimplencia]') AND objectproperty(ID,N'isProcedure') = 1)
 	DROP PROCEDURE [SP.Inadimplencia]	
 
 GO
@@ -639,7 +639,7 @@ CREATE PROCEDURE [SP.Inadimplencia]
 		INNER JOIN cliente cl
 		ON ct.id_cliente = cl.id
 		WHERE em.valor > 0 AND DAY(GETDATE()) - DAY(em.fechamento_parcela) > 0
-		ORDER BY [Nome Completo] ASC
+		ORDER BY [Nome Completo] ASC;
 		RETURN 0
 	END
 GO
@@ -653,7 +653,7 @@ GO
 
 
 
----------------------------------------SP DIVIDA CLIENTE SMN--------------------------------------------
+---------------------------------------SP DIVIDA CLIENTE BANCO--------------------------------------------
 IF EXISTS(SELECT TOP 1 1 FROM sysobjects WHERE ID = object_id(N'[SP.DividaCliente]') AND objectproperty(ID,N'isProcedure') = 1)
 	DROP PROCEDURE [SP.DividaCliente]	
 
@@ -665,7 +665,7 @@ CREATE PROCEDURE [SP.DividaCliente]
 
 	/*Documentação
 	Arquivo Fonte ......: Script.sql
-	Objetivo ...........: Apresenta o relatorio com todos os clientes com alguma inadimplencia de pagamentos de emprestimos
+	Objetivo ...........: Apresenta o relatorio de quanto um determinado cliente deve ao banco de acordo com suas dividas e taxas
 							return 0 - execuçao ok
 
 	Autor ..............: Gabriel Gouveia
@@ -687,7 +687,7 @@ CREATE PROCEDURE [SP.DividaCliente]
 		INNER JOIN cliente cl
 		ON ct.id_cliente = cl.id
 		WHERE cl.id = @id_cliente AND em.valor > 0 AND DAY(GETDATE()) - DAY(em.fechamento_parcela) > 0
-		ORDER BY [Nome Completo] ASC
+		ORDER BY [Nome Completo] ASC;
 		RETURN 0
 	END
 GO
@@ -700,6 +700,6 @@ select * from emprestimo;
 
 SELECT cl.nome, cl.sobrenome, ct.saldo FROM conta ct
 INNER JOIN cliente cl
-ON ct.id_cliente = cl.id
+ON ct.id_cliente = cl.id;
 
 --datetimenow
